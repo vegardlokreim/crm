888bd8ee0cf21dd77fc9907d8efd59c228b0013c
Vegard Lokreim <vegard.lokreim@maksimer.com>
2023-02-13 11:52:10 +0100

    added enpoint to create a company

--- server/controllers/company.js
+++ server/controllers/company.js
@@ -8,3 +8,12 @@ export const getCompanies = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const createCompany = async (req, res) => {
+    try {
+        const company = await Company.create(req.body);
+        res.status(201).json(company);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
