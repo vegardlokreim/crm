374ce1aa48551d0d0d3d5a63fcccadab03bbac02
Vegard Lokreim <vegardlokreim9@gmail.com>
2023-02-13 22:39:57 +0100

    Tesing datagrid from mui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import DataTable from "./Grid";
 
 function App() {
-
-
   const [data, setData] = useState(null);
   useEffect(() => {
     const fetchCompanies = async () => {
       const response = await axios.get("http://localhost:9000/company");
       setData(response.data);
-    }
+    };
 
     fetchCompanies();
   }, []);
   return (
     <div className="app">
-      {
-        data && data.map(comp => (
-          <h1>{comp.name.comanies}</h1>
-        ))
-      }
+      {/* {data && data.map((comp) => <h1>{comp.name}</h1>)} */}
+      <DataTable />
     </div>
   );
 }
