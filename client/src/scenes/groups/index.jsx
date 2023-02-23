ffd94189ac261709c34088b61013717d8faeede7
Vegard Lokreim <vegardlokreim9@gmail.com>
2023-02-23 13:02:55 +0100

    removing a lot of files and going back to basics again

--- client/src/scenes/groups/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Groups = () => {
-  return <div>Snart Laurdag...</div>;
-};
-
-export default Groups;
--- client/src/scenes/tickets/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Tickets = () => {
-    return (
-        <div>Tic Tac.... Caramel Toe😅</div>
-    )
-}
-
-export default Tickets
