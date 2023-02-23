684c1122477b70ab0f257b9fe0ecfa7ec82d3291
Vegard Lokreim <vegardlokreim9@gmail.com>
2023-02-23 13:13:56 +0100

    going back 2 basic

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,8 @@ import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
-import Dashboard from "scenes/dashboard";
-import Products from "scenes/products";
+
 import Customers from "scenes/customers";
-import Overview from "scenes/overview";
-import Daily from "scenes/daily";
-import Monthly from "scenes/monthly";
-import Breakdown from "scenes/breakdown";
-
-
-import Users from "scenes/users";
-import Customer from "scenes/customers";
-import CustomerOverview from "scenes/customers/[id]";
-import Groups from "scenes/groups";
-import Tasks from "scenes/tasks";
-import Tickets from "scenes/tickets";
-import UserOverview from "scenes/users/[id]";
-import Deals from "scenes/deals";
-import DealOverview from "scenes/deals/[id]";
-
-
-
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -38,23 +19,9 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/customers" replace />} />
-              {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-              {/* <Route path="/products" element={<Products />} /> */}
               <Route path="/customers" element={<Customers />} />
-              <Route path="/customer/:id" element={<CustomerOverview />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/user/:id" element={<UserOverview />} />s
-              <Route path="/groups" element={<Groups />} />
-              <Route path="/tasks" element={<Tasks />} />
-              <Route path="/tickets" element={<Tickets />} />
-              <Route path="/deals" element={<Deals />} />
-              <Route path="/deal/:id" element={<DealOverview />} />
 
 
-              {/* <Route path="/overview" element={<Overview />} />
-              <Route path="/daily" element={<Daily />} />
-              <Route path="/monthly" element={<Monthly />} />
-              <Route path="/breakdown" element={<Breakdown />} /> */}
 
 
             </Route>
